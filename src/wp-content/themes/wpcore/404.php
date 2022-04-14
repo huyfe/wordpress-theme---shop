e920b6a0429572ba1b604506d49ca83b80f72641
huy_dev <huyyntran@gmail.com>
2022-04-14 23:53:13 +0700

    Add 404 page

---
+++ src/wp-content/themes/wpcore/404.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+?>
+<main class="template-default-page">
+    <div class="container">
+        <h3 class="text-center">OOOOppppsss! Not found page!</h3>
+    </div>
+</main>
+<?php get_footer(); ?>
